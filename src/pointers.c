32c9bd59a6d17ac8d4658d142efaa50301d65387
ibandominguez <ibandominguez@live.com>
2015-12-24 14:31:49 +0000

    pointers and arrays

--- src/pointers.c
+++ src/pointers.c
@@ -12,4 +12,12 @@ int main() {
 
   printf("%p\n", &num);
   printf("%d\n", *(&num));
+
+  // arrays and pointers
+  // an array var is a pointer to its
+  // first element, if we increment it then 
+  // we will get the address of the next element and so on
+  int arr = { 1, 2 };
+  assert(arr[0] == *arr);
+  assert(arr[1] == *(arr + 1));
 }
